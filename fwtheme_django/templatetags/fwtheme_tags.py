e82a0ebc9e04a1fdec28e6cfacf3b358ed73f1db
Alex Manning <alexander.manning@stfc.ac.uk>
2024-01-04 14:13:17 +0000

    Fix loading of cookielaw.

--- fwtheme_django/templatetags/fwtheme_tags.py
+++ fwtheme_django/templatetags/fwtheme_tags.py
@@ -2,16 +2,19 @@
 """
 
 """
-__author__ = 'Richard Smith'
-__date__ = '22 Mar 2019'
-__copyright__ = 'Copyright 2018 United Kingdom Research and Innovation'
-__license__ = 'BSD - see LICENSE file in top-level package directory'
-__contact__ = 'richard.d.smith@stfc.ac.uk'
+__author__ = "Richard Smith"
+__date__ = "22 Mar 2019"
+__copyright__ = "Copyright 2018 United Kingdom Research and Innovation"
+__license__ = "BSD - see LICENSE file in top-level package directory"
+__contact__ = "richard.d.smith@stfc.ac.uk"
 
+import django.template
+import django.template.defaulttags
 from django import template
 from django.conf import settings
 
 register = template.Library()
+
 
 @register.simple_tag
 def is_container_fluid():
@@ -22,11 +25,20 @@ def is_container_fluid():
     """
     fluid = False
 
-    if hasattr(settings, 'CONTAINER_FLUID'):
+    if hasattr(settings, "CONTAINER_FLUID"):
         fluid = settings.CONTAINER_FLUID
 
     return fluid
 
 
-    # return '-fluid' if fluid else ''
-#
+@register.tag
+def load_no_raise(parser, token):
+    """Reimplement django load tag to allow failure if the library is not installed."""
+    parts = token.contents.split()
+    for name in parts[1:]:
+        try:
+            lib = django.template.defaulttags.find_library(parser, name)
+            parser.add_library(lib)
+        except django.template.TemplateSyntaxError:
+            pass
+    return django.template.defaulttags.LoadNode()
